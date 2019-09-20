17b5f7d9822c5f5ce171d1f8462922d35616a251
brandonrumer <44004200+brandonrumer@users.noreply.github.com>
2019-09-20 09:33:15 -0400

    Add files via upload

---
+++ singlecommand_v1.3.1.py
@@ -0,0 +1,470 @@
+#!/usr/local/bin/python3
+""" Summary: Connects to multiple devices and runs a command.
+
+Description:
+    Asks the user whether they want to import a CSV for IPs to work on,
+    or whether an IP range should be manually entered. Then this script
+    connects to each device and runs a user-inputted command. The output
+    is captured and exported to a CSV.
+
+    Multithreading is used so that multiple devices can be configured at
+    the same time. The user is asked how many threads are to be used so
+    the python-hosted computer or network isn't saturated.
+
+Version 1.3 Notes:
+    This version accounts for long-running commands & introduces a GUI
+    for CSV file selection.
+"""
+
+__author__ = "Brandon Rumer"
+__version__ = "1.3.1"
+__email__ = "brumer@cisco.com"
+__status__ = "Production"
+
+
+""" Importing built-in modules """
+import csv
+import socket
+import argparse
+import sys
+import tempfile
+import datetime
+import time
+import os
+import getpass
+import re
+import subprocess
+import threading
+from queue import Queue
+from multiprocessing.pool import ThreadPool
+import ipaddress
+import tkinter as tk
+from tkinter import filedialog
+
+""" Import external modules """
+import paramiko
+import requests
+#from orionsdk import SwisClient
+
+
+def ConnectIPs(startipInt, endipInt):
+    """ Collects the IPs in the range the user specified  """
+    IPs = []
+    start_ip = ipaddress.IPv4Address(startipInt)
+    end_ip = ipaddress.IPv4Address(endipInt)
+    for ip_int in range(int(start_ip), int(end_ip)):
+        i = ipaddress.IPv4Address(ip_int)
+        IPs.append(str(i))
+    return IPs
+
+       
+def ssh_exec_command(commands, host, user, pw, user_timeout, output_q):
+    """ SSH to the device, send commands, and capture the output """
+    output = ''
+    output_list = []
+    ssh_error = 'SSH Error'
+    try:
+        try:
+            # Set up SSH session
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(host, username=user, password = pw)
+            print('')
+            print('_____________________________________________________________')
+            print('')
+            print('Connection established to' , host)
+            print('_____________________________________________________________')
+            # Open Shell
+            remote_shell = ssh.invoke_shell()
+            #output = remote_shell.recv(10000)
+            time.sleep(2)
+
+            # Clear Output (banner)
+            if remote_shell.recv_ready():
+                output = remote_shell.recv(10000)
+                # Get the router/switches prompt. This will be used later to see if the commands are done.
+                deviceprompt = output.decode('ascii').rsplit('\n' , 1)[1]
+                print('device prompt var: ' , deviceprompt)
+
+            CountOfCommands = len(commands)
+
+            for i in commands:
+                deviceoutput = ''
+                cell = CountOfCommands+1 #CountOfCommands is the count, not the iteration currently on
+                # print('Output put in column ' , cell)
+                # Send the command
+                sendIt = '{}\n'.format(i)
+                remote_shell.send(sendIt)
+                time.sleep(2)
+
+                ''' Capture the screen, insuring that the command is done executing. '''
+                output = CaptureScreen(remote_shell, deviceprompt)
+                #print('Done with command number ' , cell)
+                print('Done with command')
+                print('output is: ' , output)
+                #####go back and add each command's output to a different column. Right now its all in one cell. ####
+
+
+            # Put gathered info into a row
+            output_list = [host, output] 
+            print('Adding this to report:' , output_list)
+            output_q.put([output_list])
+            
+            # Cleanup SSH
+            remote_shell.close()
+            ssh.close()
+            print('Closing SSH')
+
+        except socket.error as e:
+            ssh_error += ': Cannot connect to {} '.format(host)
+            ssh_error += 'Socket error: '
+            ssh_error += str(e)
+            print('Cannot connect to {} '.format(host) + 'Socket error: ', e)
+            ssh.close()
+            return ssh_error
+        except paramiko.AuthenticationException as e:
+            ssh_error += ': Cannot connect to %s ' % (host) + 'Authentication failed: ' + str(e)
+            print('Cannot connect to {} '.format(host) + 'Authentication failed: ', e)
+            ssh.close()
+            return ssh_error
+        except socket.timeout as e:
+            ssh_error += ': Cannot connect to %s ' % (host) + 'Socket timeout: ' + str(e)
+            print('Cannot connect to {} '.format(host) + 'Socket timeout: ', str(e))
+            ssh.close()
+            return ssh_error
+        except paramiko.SSHException as e:
+            ssh_error += ': Cannot connect to %s ' % (host) + 'SSH Exception: ' +  str(e)
+            print('Cannot connect to {} '.format(host) + 'SSH Exception: ', e)
+            ssh.close()
+            return ssh_error   
+
+    finally:
+        threadLimiter.release()
+
+
+def CaptureScreen(remote_shell, deviceprompt):
+    """ Summary: Capture last line and compare to original prompt. If they
+        match then the command is done.
+
+    Description:
+        Takes the variable from deviceprompt and compares it to the last line
+        of the screen capture. If the line doesn't match then its assumed
+        that the previous command is still executing (it is assumed that the
+        device pompt will return once the command is complete.. When the match
+        is made the last screen is returned. This is used in cases such as
+        an image upgrade on a network device where it takes several minutes to
+        complete.
+
+    Parameters:
+        deviceprompt variable. Example: Switch1#
+    """
+
+    try: 
+        # Capture the screen to see what has changed
+        if remote_shell.recv_ready():
+            outputnasty = remote_shell.recv(50000) # the output type is byte
+            # Save the last line on the screen which will be used to check command's status
+            try:
+                lastline = outputnasty.decode('ascii').rsplit('\n' , 1)[1]
+                if (lastline == None) or (lastline == ''):
+                    lastline == 'PYTHON MESSAGE: No change detected.'
+            except IndexError:
+                lastline = 'PYTHON MESSAGE: No change detected.'
+            except KeyboardInterrupt:
+                print('\n Fine. Exiting')
+                exit(0)
+        else:
+            outputnasty = 'PYTHON MESSAGE: No change detected.'
+
+        # Now we are capturing the screen FOR REPORTING, assuming the command is complete (later)
+        # Clear the first line
+        try:
+            output1 = outputnasty.decode('ascii').split('\n' , 1)[1]
+            #print('output1 var: ' , output1)
+            # Clear the last line
+            output2 = output1.rsplit('\n' , 1)[0]
+            #print('output2 var: ' , )
+            deviceoutput = output2
+            #print('deviceoutput var: ' , deviceoutput)
+        except IndexError:
+            deviceoutput = 'PYTHON MESSAGE: No change detected.'
+        except AttributeError:
+            continue
+        except KeyboardInterrupt:
+            print('\n Fine. Exiting')
+            exit(0)
+        
+        # Compare last line to the device's prompt
+        while (deviceprompt != lastline):
+            # Account for a question, notify the user, and accept the default value
+            if ('[' in lastline) and (']' in lastline):
+                print('Question detected: ' , lastline)
+                print('Sending enter to accept the default value')
+                sendenter = '\n'
+                remote_shell.send(sendenter)
+                time.sleep(2)
+                return CaptureScreen(remote_shell, deviceprompt)
+            else:
+                print('Command not done. Sleeping for 10 seconds.')
+                time.sleep(10)
+                return CaptureScreen(remote_shell, deviceprompt)
+        if deviceprompt == lastline:
+            print('Command complete!')
+            print('deviceoutput: ' , deviceoutput)
+            return deviceoutput
+
+    #except IndexError:
+        #pass
+    except KeyboardInterrupt:
+        print('\n Fine. Exiting')
+        exit(0)
+        
+
+def check_pingv2(host):
+    """ Checks to see if the IP address responds to a single ping.  """
+    with open(os.devnull, 'w') as DEVNULL:
+        try:
+            subprocess.check_call(
+                ['ping', '-n', '4', host],
+                stdout=DEVNULL,
+                stderr=DEVNULL
+            )
+            pingstatus = True
+        except subprocess.CalledProcessError:
+            pingstatus = False
+    return pingstatus
+
+
+def WorkIt(commands, host, user, pw, user_timeout, output_q):
+    """ Placeholder function, primarily needed for multithreading  """
+    pingstatus = check_pingv2(host)
+    if pingstatus == True:
+        ssh_exec_command(commands, host, user, pw, user_timeout, output_q)
+    elif pingstatus == False:
+        threadLimiter.release()
+
+
+def UserSelect():
+    """ Summary: Asks user if a CSV or manually-entered IP list should be used.
+
+    Description:
+        Asks the user whether they want to import a CSV for IPs to work on,
+        or whether an IP range should be manually entered.
+
+    Note:
+        The CSV should reside in the same directory as this .py file, be
+        named IP_Addresses.csv, and have only one column with only IPs.
+    """
+    
+    print('\n' * 2)
+    print('Would you like to import a CSV for IPs to work on, or manually')
+    print('enter an IP range?')
+    print('')
+    print('Please choose:')
+    print('Press "1" to specify an IP range')
+    print('Press "2" to specify a CSV of IPs')
+    print('Press "3" to use SolarWinds')
+    print('')
+    IPSource = input('Press 1 or 2 or 3: ')
+    if (IPSource == '1'):
+        print('')
+        return IPSource
+    elif (IPSource == '2'):
+        print('')
+        #print('Note: The CSV should reside in the same directory as this .py file, be ')
+        #print('named IP_Addresses.csv, and have only one column with only IPs.')
+        #print('')
+        #print('sleeping for 8 seconds...')
+        #time.sleep(8)
+        return IPSource
+    elif (IPSource == '3'):
+        print('')
+        return IPSource
+    else:
+        print('Syntax Error!')
+        print('\n' * 5)
+        UserSelect()
+
+
+def NumberOfCommands():
+    """ Asks the user to input a number between 1-9 """
+    try:
+        if CommandNumber >= 1:
+            return CommandNumber
+    except:
+        pass
+    try:
+        CommandNumber = int(input('How many commands (lines) do you want to run: '))
+    except ValueError:
+        print('Input a number!')
+        CommandNumber = ''
+        CommandNumber = NumberOfCommands()
+    if CommandNumber == 0:
+        print('Zero is not a valid entry')
+        CommandNumber = NumberOfCommands()
+    elif CommandNumber > 9:
+        print('For the saftely of the environment this is limited to 9 or less')
+        CommandNumber = NumberOfCommands()
+    return CommandNumber
+
+
+def MaxThreads():
+    """ Summary: Maximum threads
+
+    Description:
+        User-customizable number of maximum threads to spawn when connecting
+        to devices. This is useful so the machine and network are not
+        over-utilized when running the script.
+
+    Parameters:
+        BoundedSephamore()
+
+    Default:
+        BoundedSephamore(100)
+    """
+    
+    threads = input('Max concurrent devices do you want to connect to (default 100): ')
+    if threads == '':
+        threads = int('100')
+        return threads
+    else:
+        try:
+            number = int(threads)
+            return number
+        except ValueError:
+            print('Only input numeric numbers!')
+            print('')
+            Maxthreads()
+    
+def solarwinds_query(npm_server, username, password):
+    verify = False
+    if not verify:
+        from requests.packages.urllib3.exceptions import InsecureRequestWarning
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    swis = SwisClient(npm_server, username, password)
+    node_results = swis.query("SELECT IPAddress from Orion.Nodes n where n.Vendor = 'Cisco'")
+    return node_results
+
+def SolarwindsIP(var):
+    for devices in node_results['results']:
+    #for devices in var:
+            return devices['IPAddress']
+
+if __name__ == "__main__":
+
+    # Clearing anything so we get a clean run
+    counter = 0
+    results = []
+    my_dict = []
+    output_q = Queue()
+    user_timeout = 10000
+
+    # Defining date & time
+    today_str = str(datetime.date.today())
+    timestamp = str(today_str + '-' + (time.strftime('%H%M%S')))
+
+    # Specifying the CSV export filename
+    csvExport = 'results-{}.csv'.format(timestamp)
+    writer = csv.writer(open(csvExport, 'w', newline=''))
+    writer.writerow(['Host', 'Results'])
+   
+    # Ask the user what the souce is for devices
+    IPSource = UserSelect()
+    if IPSource == '1':
+        startipInt = input('Starting IP: ')
+        endipInt = input('Ending IP: ')
+        IPs = ConnectIPs(startipInt, endipInt)            
+    elif IPSource == '2':
+        print('CSV file should have only one column with only IPs in a single column.')
+        time.sleep(1)
+        IPs = []
+        somecsvfile = tk.Tk()
+        somecsvfile.withdraw()
+
+        filename = filedialog.askopenfilename()
+        
+        print(filename)
+
+        with open(filename , 'r') as infile:
+            reader = csv.reader(infile)
+            IPs = [rows[0] for rows in reader]
+
+    elif IPSource == '3':
+        IPs = []
+        
+        # Define solarwinds creds and connection settings
+        npm_server = input('Enter IP for SolarWinds NPM: ')
+        #npm_server = ''
+        username = input('Enter username to connect with: ')
+        #username = ''
+        # Note: Running this script in IDLE this will give an error on getpass.
+        #       This is an IDLE problem, not py problem
+        password = getpass.getpass("Enter password: ")
+        #password = ''
+
+        # Poll SolarWinds for data
+        node_results = solarwinds_query(npm_server, username, password)
+        var = node_results['results']
+
+        for IP in var:
+            IPs.append(IP['IPAddress'])
+
+        
+    # Ask how many commands to run
+    print('')
+    CommandNumber = NumberOfCommands()
+    
+    # Ask what command is to be ran on remote hosts
+    print('')
+    commands = []
+    while CommandNumber > 0:
+        print('CommandNumber: ' , CommandNumber)
+        command = input('What command do you want to run: ')
+        commands.append(command)
+        CommandNumber = CommandNumber-1
+    print('commands: ' , commands)
+    print('\n' * 2)
+    print('Please note that if "[" and "]" are detected in the same line, this script will')
+    print('assume that the device is asking a question. The default value will be chosen.')
+    print('If this is not acceptable then this script may not be suitable for the command.')
+    
+
+    # Get credentials for devices & setting some variables
+    print('\n' * 2)
+    print('Enter username to connect with.')
+    user = input('(typically, the domain is not needed): ')
+    print('')
+    # Note: Running this script in IDLE this will give an error on getpass.
+    #       This is an IDLE problem, not py problem
+    pw = getpass.getpass("Enter password: ")
+    print('\n' * 3)
+
+    # Ask user how many threads they want to spawn
+    threads = MaxThreads()
+    threadLimiter = threading.BoundedSemaphore(threads)
+
+    # Do the work, while limiting the number of threads
+    for host in IPs:
+        try:
+            threadLimiter.acquire()
+            my_thread = threading.Thread(target=WorkIt, args=(commands, host, user, pw, user_timeout, output_q))
+            my_thread.start()
+        except KeyboardInterrupt:
+            print('\n Fine. Exiting')
+            exit(0)
+
+    # Wait for threads to complete
+    main_thread = threading.currentThread()
+    for some_thread in threading.enumerate():
+        if some_thread != main_thread:
+            some_thread.join()
+
+    # Get everything from the queue
+    while not output_q.empty():
+        my_dict = output_q.get()
+        for datastuff in my_dict:
+            writer.writerow(datastuff)
+            
+    print('\n' * 5)
+    print('Results saved as:' , csvExport)
+    print('\n' * 3)
