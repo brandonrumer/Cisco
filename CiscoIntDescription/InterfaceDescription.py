588b85a53fb115b75349b9589d63306a9abf0aaf
Brandon Rumer <brumer0@gmail.com>
2021-10-27 09:17:25 -0400

    PEP8 formatting fix

--- CiscoIntDescription/InterfaceDescription.py
+++ CiscoIntDescription/InterfaceDescription.py
@@ -15,37 +15,37 @@ __status__ = "Production"
 
 """ Importing built-in modules """
 import sys
-import socket
-import os
+# import socket
+# import os
 import argparse
 import getpass
-import re
+# import re
 import time
 
 
 """ Import external modules """
-from netmiko import Netmiko, file_transfer
+from netmiko import Netmiko
+# from netmiko import file_transfer
 from paramiko.ssh_exception import SSHException
-import requests
-import textfsm
+# import requests
+# import textfsm
 
-       
+
 def ssh_exec_command(checkinterface, host, user, pw, user_timeout):
     """ SSH to the device, sshsend checkinterface, and capture the output"""
-    
+
     time.sleep(1)
     ssh_error = 'SSH Error'
 
     output = ''
-    output_list = [] # one per device
+    output_list = []  # one per device
     keys = ['Host', 'Hostname', 'Interfaces']
     device_dict = {key: None for key in keys}
     Interfaces = {}
     output_dict = {}
-    
+
     try:
         try:
-            
             # Set up SSH session
             device = {
                 'device_type': 'cisco_ios',
@@ -53,72 +53,66 @@ def ssh_exec_command(checkinterface, host, user, pw, user_timeout):
                 'username': user,
                 'password': pw,
             }
-            device_dict.update(Host = host)
-            
+            device_dict.update(Host=host)
+
             ssh = Netmiko(**device)
-            
+
             print('')
             print('_____________________________________________________________')
             print('')
-            print('Connection established to' , host)
+            print('Connection established to', host)
             print('_____________________________________________________________')
 
             time.sleep(5)
-            
+
             # Get the router/switches prompt. This will be used later to see if the checkinterface are done.
-            deviceprompt = ssh.find_prompt() #NetMiko: find device prompt
-            Hostname = deviceprompt.replace('#' , '')
-            device_dict.update(Hostname = Hostname)
-
-
+            deviceprompt = ssh.find_prompt()  # NetMiko: find device prompt
+            Hostname = deviceprompt.replace('#', '')
+            device_dict.update(Hostname=Hostname)
 
             ssh.send_command(
                 'terminal length 0\n'
             )
-            
-            #print('COMPLETE: terminal length 0')
 
-             # The string below doesn't work on slow or large stacks, so using send_command_timing instead
-            ''' # The below doesn't work on 
+            # print('COMPLETE: terminal length 0')
+
+            # The string below doesn't work on slow or large stacks, so using send_command_timing instead
+            ''' # The below doesn't work
             runningconfig = ssh.send_command(
-               'sh run | sec interface\n' ,
+               'sh run | sec interface\n',
                 expect_string=r'#'
             )
             '''
-
 
             #######################################################
             ###### COMMAND WE WANT TO LOOK AT THE OUTPUT FOR ######
             #######################################################
 
             sendcommand = 'show run interface {} | i description'.format(checkinterface)
-            #print('sendcommand: ' , sendcommand)
-            #showinterface = ssh.send_command(sendcommand , use_textfsm=True)
+            # print('sendcommand: ', sendcommand)
+            # showinterface = ssh.send_command(sendcommand, use_textfsm=True)
             showinterface = ssh.send_command(sendcommand)
-            
+
             #######################################################
             #######################################################
-            
-            
-            if (showinterface == '') or (showinterface == None):
-                interfacedescription =  'No description'
+
+            if (showinterface == '') or (showinterface is None):
+                interfacedescription = 'No description'
             else:
-                interfacedescription = showinterface.split('description ' , 1)
-                print('1:' , interfacedescription)
+                interfacedescription = showinterface.split('description ', 1)
+                print('1:', interfacedescription)
                 interfacedescription = interfacedescription[1]
 
-            printlist = [host,checkinterface,interfacedescription]
+            printlist = [host, checkinterface, interfacedescription]
             print('')
             print('On {} ({}) the interface {} has a description of "{}"'.format(host, Hostname, checkinterface, interfacedescription))
             ssh.disconnect()
-            exit(0)
+            sys.exit(0)
 
-
-            
         except IndexError:
             pass
         except SSHException:
-            print('SSH error on' , host)
+            print('SSH error on', host)
 
         except KeyboardInterrupt:
             print('\n Keyboard interrupt detected. Exiting thread.')
@@ -134,21 +128,21 @@ def ssh_exec_command(checkinterface, host, user, pw, user_timeout):
         except Exception:
             pass
 
-def main():
 
+def main():
     try:
         parser = argparse.ArgumentParser(description='Process input device and interface.')
-        parser.add_argument('-IP' , required=False, help='The IP to connect to')
+        parser.add_argument('-IP', required=False, help='The IP to connect to')
         parser.add_argument('-interface', required=False, help='The interface to check. IE: gi1/0/1, gigabitethernet1/0/1')
         parser.add_argument('-user', required=False, help='Enter username to connect with, typically the domain is not needed.')
         args = vars(parser.parse_args())
-        #print(args)
+        # print(args)
         if args['IP'] is None:
             print('')
             IP = input('Whats the IP of the device you want to check? ')
         else:
             IP = args['IP']
-            
+
         if args['interface'] is None:
             print('')
             checkinterface = []
@@ -164,17 +158,15 @@ def main():
         else:
             user = args['user']
 
-
-        #print('IP: {} & checkinterface: {}'.format(IP, checkinterface))
+        # print('IP: {} & checkinterface: {}'.format(IP, checkinterface))
 
     except KeyboardInterrupt:
         print('\n Fine. Exiting')
-        exit(0)
+        sys.exit(0)
 
     # Clearing anything so we get a clean run
     user_timeout = 10
 
-                    
     # Get credentials for devices & setting some variables
     pw = getpass.getpass("Enter password for connection (password not echod): ")
     host = IP
@@ -184,7 +176,7 @@ def main():
         ssh_exec_command(checkinterface, host, user, pw, user_timeout)
     except KeyboardInterrupt:
         print('\n Fine. Exiting')
-        exit(0)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
